646539ba9fd24d991d978969cc0407cd8d90dd95
joaojesus81 <joao.jesus@arup.com>
2020-08-07 15:09:50 +0100

    making the api endpoint work

--- api/routers/api.router.js
+++ api/routers/api.router.js
@@ -6,7 +6,7 @@ const commentsRouter = require("./comments.router");
 const { getAPIJSON } = require("../controllers/api.controller");
 const { errors405s } = require("../errors/");
 
-// apiRouter.route("/").get(getAPIJSON).all(errors405s);
+apiRouter.route("/").get(getAPIJSON).all(errors405s);
 apiRouter.use("/topics", topicsRouter);
 apiRouter.use("/users", usersRouter);
 apiRouter.use("/articles", articlesRouter);
